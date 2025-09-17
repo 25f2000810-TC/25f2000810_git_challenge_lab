1444adfd1ec38ce8612b6807fae261107340a234
Tanmay <25f2000810@ds.study.iitm.ac.in>
2025-09-17 21:30:56 +0530

    Seventh commit is done

--- sum.py
+++ sum.py
@@ -1,3 +1,6 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-print("The sum is:", a + b) 
+i = 'yes'
+while i == 'yes':
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("The sum is:", a + b) 
+    i = input("Do you want to continue? (yes/no): ")
